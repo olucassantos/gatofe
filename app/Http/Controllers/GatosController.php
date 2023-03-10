372e176db1a327ecb75d0a084ec6e6940b91a809
Lucas Anjos <oargus.g@gmail.com>
2023-03-10 20:01:12 -0300

    Criação do controller e funções

---
+++ app/Http/Controllers/GatosController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class GatosController extends Controller
+{
+    /**
+     * Lista todos os gatos cadastrados
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Mostra o formulário para criar um novo gato
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Armazena um novo gato
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Mostra um gato específico
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Mostra o formulário para editar um gato específico
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Atualiza um gato específico
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove um gato específico
+     */
+    public function destroy($id)
+    {
+
+    }
+}
