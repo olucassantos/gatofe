72be989639a7d484a73e0c6c092b1b6008e59a62
Lucas Anjos <oargus.g@gmail.com>
2023-03-10 20:15:50 -0300

    Criação do arquivo de rotas

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GatosController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,26 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Rotas para o CRUD de gatos
+
+// Rota para a listagem de gatos
+Route::get('/gatos', [GatosController::class, 'index'])->name('gatos.index');
+
+// Rota para o formulário de criação de gatos
+Route::get('/gatos/create', [GatosController::class, 'create'])->name('gatos.create');
+
+// Rota para o mostrar um gato específico
+Route::get('/gatos/{id}', [GatosController::class, 'show'])->name('gatos.show');
+
+// Rota para o formulário de edição de gatos
+Route::get('/gatos/{id}/edit', [GatosController::class, 'edit'])->name('gatos.edit');
+
+// Rota que armazena um novo gato no banco de dados
+Route::post('/gatos', [GatosController::class, 'store'])->name('gatos.store');
+
+// Rota que atualiza um gato específico no banco de dados
+Route::put('/gatos/{id}', [GatosController::class, 'update'])->name('gatos.update');
+
+// Rota que remove um gato específico do banco de dados
+Route::delete('/gatos/{id}', [GatosController::class, 'destroy'])->name('gatos.destroy');
